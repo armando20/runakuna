5f3d5a58cc3cdd3bdeb6b6f17b52e1fb65d9b03a
Jose Diaz <jose.diaz@joedayz.pe>
2017-06-25 14:33:21 -0500

    dtos para busqueda de usuarios

--- src/app/+seguridad/+busqueda-usuarios/busqueda.usuarios.component.ts
+++ src/app/+seguridad/+busqueda-usuarios/busqueda.usuarios.component.ts
@@ -4,6 +4,9 @@ import {Component, OnInit} from "@angular/core";
 import {CompleterData, CompleterService} from "ng2-completer";
 import {BackendService} from "../../+rest/backend.service";
 import {ComponentBase} from "app/+common/service/componentBase";
+import {EmpleadoService} from "../../+common/service/empleado.service";
+import {Router} from "@angular/router";
+import {UsuarioService} from "../../+common/service/usuario.service";
 
 /**
  * Created by josediaz on 6/25/17.
@@ -22,9 +25,12 @@ export class BusquedaUsuariosComponent extends ComponentBase implements OnInit {
 
     private dataServiceEmpleado:CompleterData;
 
-    constructor(public backendService: BackendService,private completerService: CompleterService){
-
-        super(backendService, 'SE001');
+    constructor(private empleadoService: EmpleadoService,
+                private _router: Router,
+                private usuarioService: UsuarioService,
+                public backendService: BackendService,
+                private completerService: CompleterService){
+        super(backendService,'SE001');
     }
 
     ngOnInit(): void {
