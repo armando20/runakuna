b90c0f97b5a5eeb84e7ae179f09c0ff4712c7578
Jose Diaz <jose.diaz@joedayz.pe>
2017-06-25 12:37:53 -0500

    routing final

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,30 +7,62 @@ import {Routes, RouterModule} from '@angular/router';
 import {MainLayoutComponent} from "./shared/layout/app-layouts/main-layout.component";
 import {AuthLayoutComponent} from "./shared/layout/app-layouts/auth-layout.component";
 import {ModuleWithProviders} from "@angular/core";
-import { LoginComponent } from "app/+auth/+login/login.component";
+import {LoginComponent} from "app/+auth/+login/login.component";
 import {AuthGuard} from "./+auth/+guards/auth.guards";
 
 export const routes: Routes = [
-  
-  {path: 'login', component: LoginComponent },
-  {
-    path: '',
-    component: MainLayoutComponent,
-    data: {pageTitle: 'Home'},
-    canActivate: [AuthGuard],
-    children: [
-      {
-        path: '', redirectTo: 'dashboard/analytics', pathMatch: 'full'
-      },
-      {
-        path: 'dashboard',
-        loadChildren: 'app/+dashboard/dashboard.module#DashboardModule',
-        data: {pageTitle: 'Dashboard'}
-      }
-    ]
-  },
 
-  {path: '**', redirectTo: 'miscellaneous/error404'}
+    {path: 'login', component: LoginComponent},
+    {
+        path: '',
+        component: MainLayoutComponent,
+        data: {pageTitle: 'Home'},
+        canActivate: [AuthGuard],
+        children: [
+            {
+                path: '', redirectTo: 'dashboard/analytics', pathMatch: 'full'
+            },
+            {
+                path: 'dashboard',
+                loadChildren: 'app/+dashboard/dashboard.module#DashboardModule',
+                data: {pageTitle: 'Dashboard'}
+            },
+            {
+                path: 'mantenimientos',
+                loadChildren: 'app/+mantenimiento/mantenimiento.module#MantenimientoModule',
+                data: {pageTitle: 'Mantenimientos'}
+            },
+            {
+                path: 'seguridad',
+                loadChildren: 'app/+seguridad/seguridad.module#SeguridadModule',
+                data: {pageTitle: 'Seguridad'}
+            },
+            {
+                path: 'personal',
+                loadChildren: 'app/+personal/personal.module#PersonalModule',
+                data: {pageTitle: 'Personal'}
+            },
+            {
+                path: 'autogestion',
+                loadChildren: 'app/+autogestion/autogestion.module#AutogestionModule',
+                data: {pageTitle: 'Autogestion'}
+            },
+            {
+                path: 'gestionTiempo',
+                loadChildren: 'app/+gestion-tiempo/gestion.tiempo.module#GestionTiempoModule',
+                data: {pageTitle: 'Gestion de Tiempo'}
+            },
+            {
+                path: 'organizacion',
+                loadChildren: 'app/+organizacion/organizacion.module#OrganizacionModule',
+                data: {pageTitle: 'Organizacion'}
+            }
+        ]
+    },
+
+    {path: 'auth', component: AuthLayoutComponent, loadChildren: 'app/+auth/auth.module#AuthModule'},
+
+    {path: '**', redirectTo: 'miscellaneous/error404'}
 
 ];
 
