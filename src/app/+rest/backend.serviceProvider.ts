781a45daae1e726f60b423a5847783348313ef84
Jose Diaz <jose.diaz@joedayz.pe>
2018-02-17 12:49:02 -0500

    puerto 7999

--- src/app/+rest/backend.serviceProvider.ts
+++ src/app/+rest/backend.serviceProvider.ts
@@ -4,7 +4,7 @@ import {AuthHttp} from "angular2-jwt";
 import {Router} from "@angular/router";
 export function provideBackendService() {
     let localhost:  String = 'localhost';
-    let port: String = '8080';
+    let port: String = '7999';
     let url = "http://" + localhost + ":" + port;
     return {
         provide: BackendService, useFactory: (http, authHttp, router) => {
